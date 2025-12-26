ea6673dde295da971f3147c0f61f5d3ecdc28ee9
Wael Kahlawy <waelkahlawi@gmail.com>
2025-12-26 20:59:53 +0200

    Update Cfg file

--- Src/Cfg.h
+++ Src/Cfg.h
@@ -11,12 +11,15 @@
 #define TAG_ADC         "ADC_DRIVER"
 #define TAG_I2C         "I2C_DRIVER"
 #define TAG_UART        "UART_DRIVER"
+#define TAG_WIFI        "WIFI_DRIVER"
 
 // Enable or disable drivers
 #define GPIO_ENABLED            STD_ON
 #define ADC_ENABLED             STD_ON
 #define I2C_ENABLED             STD_ON
 #define UART_ENABLED            STD_ON
+#define WIFI_ENABLED            STD_ON
+
 
 // Enable or disable debug logs for drivers
 // GPIO Debugging
@@ -38,6 +41,11 @@
 #define UART_DEBUG_ENABLED      STD_ON
 #endif
 
+#if WIFI_ENABLED == STD_ON
+#define WIFI_SSID               "WAEL"
+#define WIFI_PASSWORD           "Null987897"
+#define WIFI_DEBUG_ENABLED      STD_ON
+#endif
 
 
 
