f18cfe211a0038d6fb2fa3b2e9f5a330e67626a0
Wael Kahlawy <waelkahlawi@gmail.com>
2025-12-26 14:48:38 +0200

    Update ADC driver

--- Src/Hal/ADC/adc.c
+++ Src/Hal/ADC/adc.c
@@ -1,45 +1,47 @@
-#include "adc_driver.h"
+#include "adc.h" // Include ADC header
 
-#if ADC_ENABLED == STD_ON
-
-
-static const char *TAG = "ADC_DRIVER";
+#if ADC_ENABLED == STD_ON // Check if ADC feature is enabled
 
 
-void Adc_Init(Adc_ConfigType *configurations)
+static const char *g_TAG = "ADC_DRIVER"; // Debug TAG
+
+
+void Adc_Init(Adc_ConfigType *configurations) // Initialize ADC with given configurations
 {
-#if ADC_ENABLED == STD_ON
-#if ADC_DEBUG_ENABLED == STD_ON
-    ESP_LOGI(TAG, "ADC Initialized");
-    ESP_LOGI(TAG, "Sensor Channel is: %d", configurations->channel);
-    ESP_LOGI(TAG, "Sensor ADC Resolution is: %d", configurations->resolution);
+
+#if ADC_DEBUG_ENABLED == STD_ON // Debug information
+    ESP_LOGI(g_TAG, "ADC Initialized"); // Log ADC initialization
+    ESP_LOGI(g_TAG, "Sensor Channel is: %d", configurations->channel); // Log ADC channel
+    ESP_LOGI(g_TAG, "Sensor ADC Resolution is: %d", configurations->resolution); // Log ADC resolution
 #endif
     
     
-    adc1_config_width(configurations->resolution);
+    adc1_config_width(configurations->resolution); // Configure ADC resolution
     
-#if ADC_DEBUG_ENABLED == STD_ON
-    ESP_LOGI(TAG, "Sensor ADC Attenuation is: %d", configurations->attenuation);
+#if ADC_DEBUG_ENABLED == STD_ON // Debug information
+    ESP_LOGI(g_TAG, "Sensor ADC Attenuation is: %d", configurations->attenuation); // Log ADC attenuation
 #endif
     
-    adc1_config_channel_atten(configurations->channel, configurations->attenuation);
-#endif
+    adc1_config_channel_atten(configurations->channel, configurations->attenuation); // Configure ADC channel attenuation
+
 }
 
-uint32_t Adc_ReadValue(adc_channel_t channel)
+// Read value from specified ADC channel
+uint32_t Adc_ReadValue(adc_channel_t channel) 
 {
-#if ADC_ENABLED == STD_ON
+
     
-    uint32_t sensorValue = adc1_get_raw(channel);
+    uint32_t sensorValue = adc1_get_raw(channel); // Read raw ADC value from specified channel
     
 #if ADC_DEBUG_ENABLED == STD_ON
-    ESP_LOGI(TAG, "Read sensor value from channel %d and the Value is: %lu", channel, sensorValue);
+    ESP_LOGI(g_TAG, "Read sensor value from channel %d and the Value is: %lu", channel, sensorValue); // Log read sensor value
+
 #endif
-    
-    return sensorValue;
-#else
-    return 0;
-#endif
+return sensorValue; // Return the read sensor value
+
 }
+#else 
+ ESP_LOGI(g_TAG, " ADC feature is disabled "); 
+return -1;
 
 #endif 
