ad2c4cb6c9f7b5af75d666775ab5b7e1d9e47401
Abdelrahman Gamal <agmal600@gmail.com>
2025-12-29 10:14:44 +0200

    Modifing gps.h

---
+++ Src/App/GPS/gps.h
@@ -0,0 +1,20 @@
+#ifndef GPS_H_
+#define GPS_H_
+
+#include <stdio.h>
+#include <sys/_intsup.h>
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "hal/ledc_types.h"
+#include "portmacro.h"
+#include "esp_log.h"
+
+
+void GPS_Init   (void);
+void GPS_RawNmea(void);
+
+
+
+
+#endif /* GPS_H_ */
