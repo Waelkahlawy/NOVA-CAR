bc6b8f1aed23306d2478e6361e702c17eae99220
Aya-Ramadan <ayaramadan@gmail.com>
2026-01-01 16:29:34 +0200

    Handel Ultrasonic with Gpio Interrupt

--- Src/App/ULTRASONIC/ultrasonic.c
+++ Src/App/ULTRASONIC/ultrasonic.c
@@ -20,8 +20,6 @@ static volatile int64_t g_echo_start_time = 0;
 static volatile int64_t g_echo_end_time   = 0;
 static volatile uint8_t g_echo_done_flag  = 0;
 
-/* function to initialize ECHO PIN as Interrupt*/
-void Ultrasonic_EchoInterruptInit(void);
 
 /* ==================== Ultrasonic Initialization ==================== */
 void Ultrasonic_Init(Ultrasonic_Config_t *Ultrasonic_Config)
@@ -36,8 +34,8 @@ void Ultrasonic_Init(Ultrasonic_Config_t *Ultrasonic_Config)
    // --------------------Debug information----------------------------//
     #if ULTRASONIC_DEBUG_ENABLED == STD_ON 
     ESP_LOGI(g_TAG, "Trigger pin Initialized"); // Log Trigger pin initialization
-    ESP_LOGI(g_TAG, "Trigger pin is: %d", Ultrasonic_Config->trig_pin); //Trigger pin 
     #endif
+   
    
     /* Configure ECHO pin as input */
     g_Echo_Pin.pin_num   = Ultrasonic_Config->echo_pin;  // Assign ECHO GPIO number
@@ -47,19 +45,18 @@ void Ultrasonic_Init(Ultrasonic_Config_t *Ultrasonic_Config)
      // --------------------Debug information----------------------------//   
     #if ULTRASONIC_DEBUG_ENABLED == STD_ON 
     ESP_LOGI(g_TAG, "ECHO pin Initialized"); // Log ECHO pin initialization
-    ESP_LOGI(g_TAG, "ECHO pin is: %d", Ultrasonic_Config->echo_pin); //ECHO pin 
     #endif
+    
     
     /* Ensure TRIG pin is LOW before starting measurement */
     Gpio_WritePinValue(&g_Trig_Pin);  // Write LOW to TRIG pin
-    // inti ECHO as interrupt pin
-    Ultrasonic_EchoInterruptInit();
-    
+  
     // --------------------Debug information----------------------------//  
     #if ULTRASONIC_DEBUG_ENABLED == STD_ON 
     ESP_LOGI(g_TAG, "ULTRASONIC Initialized"); // Log ADC initialization
     #endif
-    return;  // Return from initialization function
+    
+ return;  // Return from initialization function
 }
 
 
@@ -116,40 +113,24 @@ static void IRAM_ATTR Ultrasonic_Echo_ISR(void *arg)
     }
 }
 
+// set callback function
 
-/* ==================== Ultrasonic ECHO Interrupt Init ==================== */
-void Ultrasonic_EchoInterruptInit(void)
+void Ultrasonic_GetEchoIsr(
+    Ultrasonic_IsrCallback *isr,
+    void **arg
+)
 {
-	// Initialize ECHO GPIO interrupt
-    gpio_config_t echo_int_conf = {
-        .pin_bit_mask = (1ULL << g_Echo_Pin.pin_num),
-        .mode = GPIO_MODE_INPUT,
-        .pull_up_en = 0,
-        .pull_down_en = 0,
-        .intr_type = GPIO_INTR_ANYEDGE   // Enable interrupt on rising and falling edges
-    };
-
-    gpio_config(&echo_int_conf);
-
-    /* Install ISR service */
-    static uint8_t isr_service_installed = 0;
-    if (!isr_service_installed)
+    if (isr != NULL)
     {
-        gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
-        isr_service_installed = 1;
+        *isr = Ultrasonic_Echo_ISR;
     }
 
-    /* Attach ISR handler */
-    gpio_isr_handler_add(
-        g_Echo_Pin.pin_num,
-        Ultrasonic_Echo_ISR,
-        NULL
-    );
-
-#if ULTRASONIC_DEBUG_ENABLED == STD_ON
-    ESP_LOGI(g_TAG, "ECHO Interrupt Initialized on GPIO %d", g_Echo_Pin.pin_num);
-#endif
+    if (arg != NULL)
+    {
+        *arg = (void *)ECHO_GPIO_PIN;
+    }
 }
+
 
 
 #endif 
